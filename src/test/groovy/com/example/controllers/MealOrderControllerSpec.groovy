4934741eb32080811cadce992068201bad6e976d
stevenmeek <stevenmeek0@outlook.com>
2024-04-06 19:42:28 +0100

    Add test for MealOrderController

---
+++ src/test/groovy/com/example/controllers/MealOrderControllerSpec.groovy
@@ -0,0 +1,28 @@
+package com.example.controllers
+
+import com.example.models.Meal
+import com.example.models.Order
+import com.example.services.IDynamoDBFacadeService
+import com.example.services.MealService
+import com.example.services.OrderService
+import spock.lang.Specification
+
+import java.security.Principal
+import java.time.Instant
+
+class MealOrderControllerSpec extends Specification {
+    def "ListAllOrdersForMeal"() {
+        given:
+        String mealId = "797b001f-de8f-47ed-833a-d84e61c73fe7"
+        IDynamoDBFacadeService dynamoDBFacadeService = Mock(IDynamoDBFacadeService)
+        OrderService orderService = new OrderService(dynamoDBFacadeService)
+        dynamoDBFacadeService.query(Order.class, _) >> [new Order(mealId: mealId, dateOfMeal: Instant.ofEpochSecond(1711487392), uid: "d84e61c73fe7-de8f-47ed-833a-797b001f")]
+        MealOrderController mealOrderController = new MealOrderController(orderService)
+
+        when:
+        List<Order> orderList = mealOrderController.listAllOrdersForMeal(mealId)
+
+        then:
+        orderList == [new Order(mealId: mealId, dateOfMeal: Instant.ofEpochSecond(1711487392), uid: "d84e61c73fe7-de8f-47ed-833a-797b001f")]
+    }
+}
