fe04ceb0e2768c51536d5196e9e59dced872c055
stevenmeek <stevenmeek0@outlook.com>
2024-05-27 12:13:36 +0100

    Add script to wait for localstack to start up.

---
+++ scripts/wait_for_localstack_init_script_to_finish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+result=$(curl -s localhost:4566/_localstack/init | jq -r ".scripts[0].state")
+while [ "$result" == "RUNNING" ]; do
+  result=$(curl -s localhost:4566/_localstack/init | jq -r ".scripts[0].state")
+  if [ "$result" == "SUCCESSFUL" ]; then
+      echo "Init script finished SUCCESSFUL"
+      exit 1
+  elif [ "$result" == "RUNNING" ]; then
+      echo "Init script is still $result"
+      sleep 10
+  elif [ "$result" == "ERROR" ]; then
+      echo "Error in localstack init script, exit code 0"
+      exit 0
+  else
+    echo "Error in script."
+    exit 0
+  fi
+done
+echo "ERROR in init script"
+exit 0
