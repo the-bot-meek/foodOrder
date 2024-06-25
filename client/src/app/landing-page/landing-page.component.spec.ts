9677e4d9433345a97d873f3f0a96caa8b4ec0e20
stevenmeek <stevenmeek0@outlook.com>
2024-06-25 22:53:03 +0100

    feat(client): Add meal table to landing-page component.

--- client/src/app/landing-page/landing-page.component.spec.ts
+++ client/src/app/landing-page/landing-page.component.spec.ts
@@ -4,6 +4,9 @@ import { LandingPageComponent } from './landing-page.component';
 import {MealService} from "../shared/api/meal.service";
 import {IMeal} from "../../../models/meal";
 import {of} from "rxjs";
+import {TestbedHarnessEnvironment} from "@angular/cdk/testing/testbed";
+import {HarnessLoader} from "@angular/cdk/testing";
+import {MatRowHarness, MatTableHarness} from "@angular/material/table/testing";
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
@@ -16,8 +19,14 @@ describe('LandingPageComponent', () => {
     mealDate: 1718488580592,
     location: 'London',
     sortKey: 'sortKey": "2024-06-15T21:56:20.592Z_07405c0f-7f50-4e78-82b0-78f8d730d42b',
-    primaryKey: '7f50-07405c0f-4e78-82b0-78f8d730d42b'
+    primaryKey: '7f50-07405c0f-4e78-82b0-78f8d730d42b',
+    venueName: 'MacD',
+    mealConfig: {
+      draft: false,
+      privateMealConfig: undefined
+    }
   }]
+  let loader: HarnessLoader
 
   beforeEach(async () => {
     mealService = {listMeal: jasmine.createSpy().and.returnValue(of(meals))}
@@ -32,10 +41,27 @@ describe('LandingPageComponent', () => {
 
     fixture = TestBed.createComponent(LandingPageComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add item to table', async () => {
+    const matTableHarness: MatTableHarness = await loader.getHarness<MatTableHarness>(MatTableHarness.with({
+      selector: '#meal-table'
+    }))
+    const rows: MatRowHarness[] = await matTableHarness.getRows()
+
+    console.log(await rows[0].getCellTextByColumnName())
+    expect(await rows[0].getCellTextByColumnName()).toEqual({
+      name: 'name',
+      mealDate: 'Jun 15, 2024',
+      location: 'London',
+      venueName: 'MacD'
+    })
+    expect(rows.length).toEqual(1)
+  })
 });
--- client/src/app/landing-page/landing-page.component.ts
+++ client/src/app/landing-page/landing-page.component.ts
@@ -2,20 +2,24 @@ import {Component, OnInit} from '@angular/core';
 import {MealService} from "../shared/api/meal.service";
 import {Observable} from "rxjs";
 import {IMeal} from "../../../models/meal";
-import {AsyncPipe, JsonPipe} from "@angular/common";
+import {AsyncPipe, DatePipe, JsonPipe} from "@angular/common";
+import {MatTableModule} from "@angular/material/table";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
     AsyncPipe,
-    JsonPipe
+    JsonPipe,
+    MatTableModule,
+    DatePipe
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent implements OnInit {
   meals: Observable<IMeal[]>
+  displayedColumns: string[] = ['name', 'mealDate', 'location', 'venueName']
   constructor(private mealService: MealService) {
   }
 
