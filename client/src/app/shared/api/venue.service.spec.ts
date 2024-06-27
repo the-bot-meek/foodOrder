a9d794ef0d3d65a17c14947a993c52093ec95e12
stevenmeek <stevenmeek0@outlook.com>
2024-06-27 22:13:57 +0100

    chore(client): Upgrade to angular 18.

--- client/src/app/shared/api/venue.service.spec.ts
+++ client/src/app/shared/api/venue.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { VenueService } from './venue.service';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 describe('VenueService', () => {
   let service: VenueService;
