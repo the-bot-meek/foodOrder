dae2e598dda0dc9ebd47d245b9b0b480017725ae
stevenmeek <stevenmeek0@outlook.com>
2024-08-17 16:59:01 +0100

    Make repo multi project.

---
+++ infra/src/main/java/com/example/AppStack.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import io.micronaut.aws.cdk.function.MicronautFunction;
+import io.micronaut.aws.cdk.function.MicronautFunctionFile;
+import io.micronaut.starter.application.ApplicationType;
+import io.micronaut.starter.options.BuildTool;
+import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.lambda.Architecture;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Tracing;
+import software.constructs.Construct;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AppStack extends Stack {
+
+    public AppStack(final Construct parent, final String id) {
+        this(parent, id, null);
+    }
+
+    public AppStack(final Construct parent, final String id, final StackProps props) {
+        super(parent, id, props);
+
+        Map<String, String> environmentVariables = new HashMap<>();
+    }
+
+    public static String functionPath() {
+        return "../app/build/libs/" + functionFilename();
+    }
+
+    public static String functionFilename() {
+        return MicronautFunctionFile.builder()
+            .optimized()
+            .graalVMNative(false)
+            .version("0.1")
+            .archiveBaseName("app")
+            .buildTool(BuildTool.GRADLE)
+            .build();
+    }
+}
