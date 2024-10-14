8cdeff1d2f265d43e52f3b5ac6b776d58f3bb7e8
stevenmeek <stevenmeek0@outlook.com>
2024-10-14 22:07:04 +0100

    chore(test): Add test_user_session cookie value to requests made with a user agent to help with isolating test by using the Venue stored in the cookie as the Authentication.name value in the MockAuthenticationFetcher class.

--- src/main/java/com/example/mocks/MockAuthenticationFetcher.java
+++ src/main/java/com/example/mocks/MockAuthenticationFetcher.java
@@ -1,8 +1,9 @@
-package com.example.Mocks;
+package com.example.mocks;
 
 import io.micronaut.context.annotation.Replaces;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.cookie.Cookie;
 import io.micronaut.security.authentication.Authentication;
 import io.micronaut.security.authentication.ServerAuthentication;
 import jakarta.inject.Singleton;
@@ -16,10 +17,20 @@ import java.util.Map;
 @Requires(env = "mock_auth")
 @Replaces(AuthenticationFetcher.class)
 public class MockAuthenticationFetcher implements AuthenticationFetcher<HttpRequest<?>> {
+    private final MockAuthConfig mockAuthConfig;
+    MockAuthenticationFetcher(MockAuthConfig mockAuthConfig) {
+        this.mockAuthConfig = mockAuthConfig;
+    }
+
     @Override
     public Publisher<Authentication> fetchAuthentication(HttpRequest<?> request) {
+
+        Cookie testUserIdCookie = request.getCookies().get("test_user_session");
+        if (testUserIdCookie == null || !mockAuthConfig.isSessionLessStateAuth()) {
+            testUserIdCookie = Cookie.of("test_user_session", "steven");
+        }
         Map<String, Object> attributes = Map.of("name", "The bot meek");
-        Authentication authentication = new ServerAuthentication("steven", null, attributes);
+        Authentication authentication = new ServerAuthentication(testUserIdCookie.getValue(), null, attributes);
         return Mono.just(authentication);
     }
 }
