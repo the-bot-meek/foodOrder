2265b8541c252eb8d337096e3ee81ba2982c30b9
stevenmeek <stevenmeek0@outlook.com>
2024-10-14 23:04:33 +0100

    chore(test): Rename stateless-session-auth env var to STATELESS_SESSION_AUTH to comply with standards and add STATELESS_SESSION_AUTH env var to docker compose file.

--- src/main/java/com/example/mocks/MockAuthConfig.java
+++ src/main/java/com/example/mocks/MockAuthConfig.java
@@ -2,14 +2,14 @@ package com.example.mocks;
 
 import io.micronaut.context.annotation.ConfigurationProperties;
 
-@ConfigurationProperties("mock-auth-config")
+@ConfigurationProperties("mockAuthConfig")
 public class MockAuthConfig {
     /**
      * When sessionLessStateAuth is true, a new test session id will be generated for each session.
      * The generated session id is then used as the name in the Authentication object used by the application.
      * This allows us to isolate automated e2e tests to allow for more consist results
      */
-    private boolean sessionLessStateAuth = false;
+    private boolean sessionLessStateAuth = true;
 
     public boolean isSessionLessStateAuth() {
         return sessionLessStateAuth;
