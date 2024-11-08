71a885c9dda8af9627d1850d02a9107d8d4dcf62
stevenmeek <stevenmeek0@outlook.com>
2024-11-08 21:25:15 +0000

    Chore(server) - docker swarm.

--- server/src/main/java/com/example/mocks/TestIdInterceptor.java
+++ server/src/main/java/com/example/mocks/TestIdInterceptor.java
@@ -8,6 +8,8 @@ import io.micronaut.http.cookie.Cookie;
 import io.micronaut.http.filter.HttpServerFilter;
 import io.micronaut.http.filter.ServerFilterChain;
 import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 import java.util.UUID;
 
@@ -16,6 +18,7 @@ import java.util.UUID;
 @Requires(env = "mock_auth")
 @Requires(property = "mockAuthConfig.sessionLessStateAuth", value = "true")
 public class TestIdInterceptor implements HttpServerFilter {
+    public static final Logger log = LoggerFactory.getLogger(TestIdInterceptor.class);
     @Override
     public Publisher<MutableHttpResponse<?>> doFilter(HttpRequest<?> request, ServerFilterChain chain) {
         // If a user-Agent is not provided, then we will assume that it is a unit test and proceed without the test_user_session cookie
@@ -26,10 +29,12 @@ public class TestIdInterceptor implements HttpServerFilter {
         boolean hasCookieBeenAdded = testUserIdCookie != null;
         if (!hasCookieBeenAdded) {
             testUserIdCookie = Cookie.of("test_user_session", UUID.randomUUID().toString());
+            log.debug("Setting testUserIdCookie: {}", testUserIdCookie.getValue());
             request = request.mutate().cookie(testUserIdCookie);
         }
 
         final Cookie finalTestUserIdCookie = testUserIdCookie;
+        log.debug("Processing request with testUserIdCookie: {}", finalTestUserIdCookie.getValue());
         return Mono.from(chain.proceed(request)).map((MutableHttpResponse<?> resp) -> {
             if(!hasCookieBeenAdded) {
                 resp.cookie(finalTestUserIdCookie);
