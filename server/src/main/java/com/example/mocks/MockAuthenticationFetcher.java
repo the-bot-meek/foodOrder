0029c729f5e13e3cb1f5106fd75a3bb7d0e07d35
stevenmeek <stevenmeek0@outlook.com>
2024-11-08 21:25:15 +0000

    Chore(server) - docker swarm.

--- server/src/main/java/com/example/mocks/MockAuthenticationFetcher.java
+++ server/src/main/java/com/example/mocks/MockAuthenticationFetcher.java
@@ -8,6 +8,8 @@ import io.micronaut.security.authentication.Authentication;
 import io.micronaut.security.authentication.ServerAuthentication;
 import jakarta.inject.Singleton;
 import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 import io.micronaut.security.filters.AuthenticationFetcher;
 
@@ -17,6 +19,7 @@ import java.util.Map;
 @Requires(env = "mock_auth")
 @Replaces(AuthenticationFetcher.class)
 public class MockAuthenticationFetcher implements AuthenticationFetcher<HttpRequest<?>> {
+    public static final Logger log = LoggerFactory.getLogger(MockAuthenticationFetcher.class);
     private final MockAuthConfig mockAuthConfig;
     MockAuthenticationFetcher(MockAuthConfig mockAuthConfig) {
         this.mockAuthConfig = mockAuthConfig;
@@ -24,11 +27,12 @@ public class MockAuthenticationFetcher implements AuthenticationFetcher<HttpRequ
 
     @Override
     public Publisher<Authentication> fetchAuthentication(HttpRequest<?> request) {
-
+        log.debug("Fetching mock authentication for {}", request.getUri());
         Cookie testUserIdCookie = request.getCookies().get("test_user_session");
         if (testUserIdCookie == null || !mockAuthConfig.isSessionLessStateAuth()) {
             testUserIdCookie = Cookie.of("test_user_session", "steven");
         }
+        log.debug("Test user session cookie is {}", testUserIdCookie.getValue());
         Map<String, Object> attributes = Map.of("name", "The bot meek");
         Authentication authentication = new ServerAuthentication(testUserIdCookie.getValue(), null, attributes);
         return Mono.just(authentication);
