80a88581673bc0a0e83725616791b8ae67aff32a
stevenmeek <stevenmeek0@outlook.com>
2024-11-14 22:58:39 +0000

    Issue(ci) - Fix npm ci error in localstack init script and dump localstack logs on failure in gradle tests.

--- local_stack/init-aws.sh
+++ local_stack/init-aws.sh
@@ -2,7 +2,7 @@
 # This is not intended to be run by a developer. This script is used to run the CDK project inside of local stack
 # This script doesn't work with the community edition of localstack if PERSISTENCE is enabled as cloudformation stacks aren't persisted
 chmod -R 777 /etc/localstack/init/ready.d
-npm --prefix ./CDK ci
+npm --prefix /CDK ci
 has_bootstrap_run=$(awslocal cloudformation describe-stacks --region eu-west-1 --query "contains(Stacks[*].StackName,'CDKToolkit')")
 if [ "$has_bootstrap_run" == "false" ];
   then npm --prefix /CDK run localstack-bootstrap;
