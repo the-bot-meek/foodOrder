41fb01d6f73c6391ae7683a7cce53afdc5df47a6
stevenmeek <stevenmeek0@outlook.com>
2024-11-14 22:47:32 +0000

    Issue(dev) - Fix issue with the localstack init script trying to create the DynamoDB tables twice.
    Bug description:
    When restarting the localstack container, the init script would error due to the fact that the cdk app would bootstrap the application every time.
    To fix this, I added a condition to the init script to only run the bootstrap command when the CDKToolkit stack has not been added.
    Unfortunately cdk stack are not persisted by the community edition of localstack, so I have had to remove the resistance entirely.

--- local_stack/init-aws.sh
+++ local_stack/init-aws.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # This is not intended to be run by a developer. This script is used to run the CDK project inside of local stack
-npm install -g aws-cdk-local aws-cdk
+# This script doesn't work with the community edition of localstack if PERSISTENCE is enabled as cloudformation stacks aren't persisted
 chmod -R 777 /etc/localstack/init/ready.d
-npm --prefix /CDK ci
-npm --prefix /CDK run localstack-bootstrap
+npm --prefix ./CDK ci
+has_bootstrap_run=$(awslocal cloudformation describe-stacks --region eu-west-1 --query "contains(Stacks[*].StackName,'CDKToolkit')")
+if [ "$has_bootstrap_run" == "false" ];
+  then npm --prefix /CDK run localstack-bootstrap;
+fi
 npm --prefix /CDK run clocalstack-deploy
