c031ed2b00baa92d2e235718dc7bc4c2da379ee1
stevenmeek <stevenmeek0@outlook.com>
2024-11-14 12:48:35 +0000

    Chore(ci) - Add aws-cdk-local as a dev to pacakge.json as a dev dependency, so we can track which version we are using in git.

--- local_stack/init-aws.sh
+++ local_stack/init-aws.sh
@@ -2,4 +2,6 @@
 # This is not intended to be run by a developer. This script is used to run the CDK project inside of local stack
 npm install -g aws-cdk-local aws-cdk
 chmod -R 777 /etc/localstack/init/ready.d
-(cd /CDK || return; npm install; cdklocal bootstrap; cdklocal deploy)
+npm --prefix /CDK ci
+npm --prefix /CDK run localstack-bootstrap
+npm --prefix /CDK run clocalstack-deploy
