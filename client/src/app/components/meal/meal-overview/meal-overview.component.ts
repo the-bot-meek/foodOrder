a70a7173b24cb2ab5c846c9e245e494bc3578654
stevenmeek <stevenmeek0@outlook.com>
2024-11-21 20:20:28 +0000

    Chore(client) - Remove unnecessary imports from angular components.

--- client/src/app/components/meal/meal-overview/meal-overview.component.ts
+++ client/src/app/components/meal/meal-overview/meal-overview.component.ts
@@ -3,18 +3,11 @@ import {ActivatedRoute} from "@angular/router";
 import {MealService} from "../../../shared/api/meal.service";
 import {Observable} from "rxjs";
 import {IMeal} from "../../../../../models/meal";
-import {AsyncPipe, DatePipe, JsonPipe, NgIf} from "@angular/common";
-import {MatButton} from "@angular/material/button";
 import {MealDetailsComponent} from "../meal-details/meal-details.component";
 
 @Component({
     selector: 'app-overview',
     imports: [
-        JsonPipe,
-        AsyncPipe,
-        NgIf,
-        DatePipe,
-        MatButton,
         MealDetailsComponent
     ],
     templateUrl: './meal-overview.component.html',
