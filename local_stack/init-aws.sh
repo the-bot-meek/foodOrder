381a328f2184863284d3b2cfffa49fb1ee3f4bc0
stevenmeek <stevenmeek0@outlook.com>
2025-01-02 16:18:35 +0000

    Chore(localstack): Set default account to work with a=the new cdk version.

--- local_stack/init-aws.sh
+++ local_stack/init-aws.sh
@@ -3,8 +3,14 @@
 # This script doesn't work with the community edition of localstack if PERSISTENCE is enabled as cloudformation stacks aren't persisted
 chmod -R 777 /etc/localstack/init/ready.d
 npm --prefix /CDK ci
+
+aws configure set aws_access_key_id "1234"
+aws configure set aws_secret_access_key "1234"
+aws configure set default.region "local"
+
+
 has_bootstrap_run=$(awslocal cloudformation describe-stacks --region eu-west-1 --query "contains(Stacks[*].StackName,'CDKToolkit')")
 if [ "$has_bootstrap_run" == "false" ];
   then npm --prefix /CDK run localstack-bootstrap;
 fi
-npm --prefix /CDK run clocalstack-deploy
+npm --prefix /CDK run localstack-deploy
