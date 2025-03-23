5d4528008f448cf6649b03191463e88f546d365d
stevenmeek <stevenmeek0@outlook.com>
2025-03-23 10:18:04 +0000

    chore(cdk) refactor cdk stack into prod and dev.

--- CDK/lib/cdk-stack.ts
+++ CDK/lib/cdk-stack.ts
@@ -5,6 +5,7 @@ import {ProjectionType} from 'aws-cdk-lib/aws-dynamodb'
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import {Tags} from "aws-cdk-lib";
+import {GatewayConstruct} from "./gateway-construct";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -33,32 +34,17 @@ export class CdkStack extends cdk.Stack {
       }]
     })
 
-    const micronautLambda = new lambda.Function(this, 'MicronautLambda', {
-      runtime: lambda.Runtime.JAVA_21, // Micronaut typically uses Java; adjust if necessary.
-      handler: 'io.micronaut.function.aws.proxy.payload1.ApiGatewayProxyRequestEventFunction::handleRequest', // Change this to your actual handler.
-      code: lambda.Code.fromAsset('/builds/restApiHandler/lambda-handler-1.0.0-all.jar'), // The directory containing your packaged JAR.
-      memorySize: 1024,
-      timeout: cdk.Duration.seconds(10),
-      environment: {
+    const gatewayConstruct: GatewayConstruct = new GatewayConstruct(this, 'GatewayConstruct', {
+      lambdaEnvs: {
         "MICRONAUT_ENVIRONMENTS": "mock_auth",
         "STATELESS_SESSION_AUTH": "false",
         "DYNAMODB_DOMAIN": "localstack"
       }
-    });
-
-    Tags.of(micronautLambda).add('_custom_id_', 'test-lambda-rul')
-
-    micronautLambda.addFunctionUrl()
-
-
-    const restApi= new apigw.LambdaRestApi(this, 'MicronautApi', {
-      handler: micronautLambda,
-      proxy: true,
-    });
+    })
 
     // This tag allows us to use a static endpoint to call the api
     // Eg: http://localhost:4566/_aws/execute-api/api/prod
     // See: https://docs.localstack.cloud/user-guide/aws/apigateway/#:~:text=Copy-,Custom%20IDs%20for%20API%20Gateway%20resources%20via%20tags,-You%20can%20assign
-    Tags.of(restApi).add('_custom_id_', 'api')
+    Tags.of(gatewayConstruct.restApi).add('_custom_id_', 'api')
   }
 }
