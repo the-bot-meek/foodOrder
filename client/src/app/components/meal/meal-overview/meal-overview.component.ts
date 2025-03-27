fbcff2dde66f38094d167322ac7b869f092863fe
stevenmeek <stevenmeek0@outlook.com>
2025-03-27 20:14:36 +0000

    feat(client) add the ability to view all orders for a meal.

--- client/src/app/components/meal/meal-overview/meal-overview.component.ts
+++ client/src/app/components/meal/meal-overview/meal-overview.component.ts
@@ -6,12 +6,13 @@ import {IMeal} from "@the-bot-meek/food-orders-models/models/meal";
 import {MealDetailsComponent} from "../meal-details/meal-details.component";
 
 @Component({
-    selector: 'app-overview',
-    imports: [
-        MealDetailsComponent
-    ],
-    templateUrl: './meal-overview.component.html',
-    styleUrl: './meal-overview.component.scss'
+  selector: 'app-overview',
+  imports: [
+    MealDetailsComponent
+  ],
+  templateUrl: './meal-overview.component.html',
+  standalone: true,
+  styleUrl: './meal-overview.component.scss'
 })
 export class MealOverviewComponent implements OnInit {
   meal: Observable<IMeal>;
