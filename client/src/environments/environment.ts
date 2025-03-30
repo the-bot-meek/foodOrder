2eed72bb71091a54ab7dec2e1d17c2c6e6e0904d
stevenmeek <stevenmeek0@outlook.com>
2025-03-30 16:52:42 +0100

    chore(server) Add default client base path to allow an order link to work.

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     serverUrl: "http://localhost:8080",
-    authPath: 'oauth/login/auth0'
+    authPath: 'oauth/login/auth0',
+    defaultClientBasePath: 'http://localhost:4200'
 };
