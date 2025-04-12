2fd494ac35d809ba3119bc6e0292de2c8fc39e78
stevenmeek <stevenmeek0@outlook.com>
2025-04-12 20:36:39 +0100

    chore(e2e) refactor e2e tests to utilise helper methods to help speed up testing.

--- client/e2e/landing-page.spec.ts
+++ client/e2e/landing-page.spec.ts
@@ -6,6 +6,7 @@ import {
 } from "./utils";
 import {ICreateMenuRequest} from "@the-bot-meek/food-orders-models/models/menu";
 import {ICreateMealRequest} from "@the-bot-meek/food-orders-models/models/ICreateMealRequest";
+import {MealConfig} from "@the-bot-meek/food-orders-models/models/MealConfig";
 
 test('has title', async ({ page }) => {
   await page.goto('/');
@@ -23,8 +24,13 @@ test('add menu test', async ({ page, browserName }, testInfo: TestInfo) => {
 
 test('test add meal', async ({ page, browserName }, testInfo: TestInfo) => {
   await page.goto('/');
+
+  const mealConfig: MealConfig = {
+      draft: false,
+      privateMealConfig: undefined
+  }
   const createMenuRequest: ICreateMenuRequest = buildCreateMenuRequest(testInfo, browserName)
-  const createMealRequest: ICreateMealRequest = buildCreateMealRequest(testInfo, browserName)
+  const createMealRequest: ICreateMealRequest = buildCreateMealRequest(mealConfig, testInfo, browserName)
   await addMenu(page, createMenuRequest)
   await addMeal(page, createMealRequest)
   await verifyMealTableRowExists(page, createMealRequest)
