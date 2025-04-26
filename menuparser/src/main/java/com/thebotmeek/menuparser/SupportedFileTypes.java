af003305150c1b5a9c089b1a79ee5ab713fd555f
stevenmeek <stevenmeek0@outlook.com>
2025-04-26 16:28:32 +0100

    Add logic to parse a DOCX document to raw text.

--- menuparser/src/main/java/com/thebotmeek/menuparser/SupportedFileTypes.java
+++ menuparser/src/main/java/com/thebotmeek/menuparser/SupportedFileTypes.java
@@ -1,9 +1,5 @@
 package com.thebotmeek.menuparser;
 
 public enum SupportedFileTypes {
-    DOCX("docx");
-
-    SupportedFileTypes(String docx) {
-
-    }
+    DOCX;
 }
--- menuparser/src/main/java/com/thebotmeek/menuparser/exception/MenuParserException.java
+++ menuparser/src/main/java/com/thebotmeek/menuparser/exception/MenuParserException.java
@@ -4,4 +4,7 @@ public class MenuParserException extends Exception {
     public MenuParserException(String userId, String taskId, Exception cause) {
         super(String.format("User %s, Task %s: %s", userId, taskId, cause));
     }
+
+    public MenuParserException() {
+    }
 }
