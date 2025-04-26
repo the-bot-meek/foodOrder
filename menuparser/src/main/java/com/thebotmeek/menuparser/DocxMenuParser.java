1f43bf9912d093a78f5465cb73697a43d11f6f65
stevenmeek <stevenmeek0@outlook.com>
2025-04-26 16:51:03 +0100

    Add controller and llm integration.

--- menuparser/src/main/java/com/thebotmeek/menuparser/DocxMenuParser.java
+++ menuparser/src/main/java/com/thebotmeek/menuparser/DocxMenuParser.java
@@ -2,25 +2,36 @@ package com.thebotmeek.menuparser;
 
 import com.foodorder.server.models.MenuItem;
 import com.thebotmeek.menuparser.exception.MenuParserException;
+import io.micronaut.serde.ObjectMapper;
 import jakarta.inject.Named;
 import jakarta.inject.Qualifier;
 import jakarta.inject.Singleton;
+import jakarta.validation.Valid;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
 
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Singleton
 @Named("DOCX")
 public class DocxMenuParser implements MenuParser {
+    TextToMenuItemParser textToMenuItemParser;
+    ObjectMapper objectMapper;
+
+    DocxMenuParser(TextToMenuItemParser textToMenuItemParser, ObjectMapper objectMapper) {
+        this.textToMenuItemParser = textToMenuItemParser;
+    }
+
     @Override
-    public Set<MenuItem> parse(MenuParseEvent menuParseEvent) throws MenuParserException {
+    public @Valid Set<MenuItem> parse(MenuParseEvent menuParseEvent) throws MenuParserException {
         try  {
             String docAsTest = getDocumentAsRawText(menuParseEvent.getInputStream());
-            return Set.of();
+            String menuItemsJson = textToMenuItemParser.parseMenu(docAsTest);
+            return Arrays.stream(objectMapper.readValue(menuItemsJson, MenuItem[].class)).collect(Collectors.toSet());
         } catch (Exception e) {
             throw new MenuParserException(
                     menuParseEvent.getMenuParseTask().getUserId(),
