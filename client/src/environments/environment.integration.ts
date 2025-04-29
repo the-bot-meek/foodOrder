6a25d696de7872841e47d84d9e5f8cf12b2562a4
stevenmeek <stevenmeek0@outlook.com>
2025-04-29 21:47:00 +0100

    Update docker compose to run the entire application.

--- client/src/environments/environment.integration.ts
+++ client/src/environments/environment.integration.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  serverUrl: "http://localhost/api",
-  menuParserUrl: "http://localhost/api/menuParseTask",
+  serverUrl: "/api",
+  menuParserUrl: "/api/menuParseTask",
   authPath: 'oauth/login/auth0',
   defaultClientBasePath: 'http://localhost'
 };
